90def937f593635423ce0cc414577d79bfb445dd
syoch <syoch64@gmail.com>
2022-07-04 10:25:37 +0900

    update: some

--- src/apps/calc/index.svelte
+++ src/apps/calc/index.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-  // import Fab, { Icon } from "@smui/fab";
   import Button, { Icon } from "@smui/button";
+
+  let log: String[] = [];
+  let formula: string = "";
+
+  $: console.log(log);
 
   const html_mapping = {
     and: "&#x2227;",
@@ -9,12 +13,9 @@
     lsht: "&#x2A02;",
     rsht: "&#x2192;",
     not: "&#x00AC;",
-    DIV: "&#x00F7;",
-    MOD: "&#x2225;",
-    MUL: "&#x00D7;",
-    ADD: "&#x002B;",
-    SUB: "&#x2212;",
     PARENTSIS: "( )",
+  };
+  const text_mapping = {
     ONE: "1",
     TWO: "2",
     THREE: "3",
@@ -26,6 +27,51 @@
     NINE: "9",
     ZERO: "0",
     DOT: ".",
+
+    DIV: "/",
+    MUL: "*",
+    SUB: "-",
+    ADD: "+",
+
+    xor: "^",
+    or: "|",
+    and: "&",
+    lsht: "<<",
+    rsht: ">>",
+    not: "~",
+
+    neg1: " as neg1",
+    neg2: " as neg2",
+    negs: " as negs",
+  };
+  const display_mapping = {
+    ONE: "1",
+    TWO: "2",
+    THREE: "3",
+    FOUR: "4",
+    FIVE: "5",
+    SIX: "6",
+    SEVEN: "7",
+    EIGHT: "8",
+    NINE: "9",
+    ZERO: "0",
+    DOT: ".",
+
+    DIV: "/",
+    MUL: "*",
+    SUB: "-",
+    ADD: "+",
+
+    xor: "^",
+    or: "|",
+    and: "&",
+    lsht: "<<",
+    rsht: ">>",
+    not: "~",
+
+    neg1: " as neg1",
+    neg2: " as neg2",
+    negs: " as negs",
   };
   const icon_mapping = {
     BS: "backspace",
@@ -43,9 +89,9 @@
 
 <div style="height: 30%; position: relative;">
   <div style="position: absolute; bottom: 30%;">
-    LOG <br />
-    LOG2 <br />
-    LOG3 <br />
+    {#each log as line}
+      {line} <br />
+    {/each}
   </div>
   <span
     style:position="absolute"
@@ -54,7 +100,7 @@
     style:transform="translate(-50%,-50%)"
     style:font-size="x-large"
   >
-    hello world!
+    {formula}
   </span>
   <span
     style:position="absolute"
@@ -72,9 +118,16 @@
       <Button
         style="grid-row: {r + 1}; grid-column: {c + 1}; height: 100%;"
         variant="text"
+        on:click={() => {
+          formula += display_mapping[cell] || ` ${cell} `;
+          log.push(cell);
+          log = log;
+        }}
       >
         {#if cell in html_mapping}
           <span style="font-size: 5vw;">{@html html_mapping[cell]}</span>
+        {:else if cell in text_mapping}
+          <span style="font-size: 5vw;">{@html text_mapping[cell]}</span>
         {:else if cell in icon_mapping}
           <Icon class="material-icons" style="font-size: 5vw;"
             >{icon_mapping[cell]}</Icon
