0e1e72a2a9431bc3c004b503c3dd3a722991b6ad
syoch <hacksyoch@gmail.com>
2022-09-02 16:56:51 +0900

    wip

--- src/libs/old/time.js
+++ src/libs/old/time.js
@@ -1,11 +1,11 @@
 function getime(mode) {
-  let jikan = new Date();
-  let hour = jikan.getHours();
-  let minu = jikan.getMinutes();
-  let seco = jikan.getSeconds();
-  let year = jikan.getFullYear();
-  let mont = jikan.getMonth() + 1;
-  let daay = jikan.getDate();
+  let date_time = new Date();
+  let hour = date_time.getHours();
+  let minu = date_time.getMinutes();
+  let seco = date_time.getSeconds();
+  let year = date_time.getFullYear();
+  let mont = date_time.getMonth() + 1;
+  let daay = date_time.getDate();
   let th = ('00' + hour).slice(-2);
   let tm = ('00' + minu).slice(-2);
   let ts = ('00' + seco).slice(-2);
