db9a7bb23093a054ce6cbd4c523b2d9bed8f20ef
syoch <syoch64@gmail.com>
2022-09-20 16:08:57 +0900

    Update: Added target

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [svelte()],
   resolve: {
     alias: {
