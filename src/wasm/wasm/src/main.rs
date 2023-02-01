b8b08c9456941513d6eacf1ade13445f873ff34a
syoch <syoch64@gmail.com>
2023-02-02 04:08:17 +0900

    update: make test as module test

--- src/wasm/wasm/src/main.rs
+++ src/wasm/wasm/src/main.rs
@@ -1,26 +1,32 @@
 extern crate kernel;
 extern crate python;
 
-use python::PythonProcess;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut k = kernel::Kernel::default();
-    let p = PythonProcess::new(
-        r#"async def proc():
-    print("print")
-    print("/")
-
-    print("step")
-    await step()
-
-    print("pending")
-    await pending()
-
-wrapper()"#
-            .to_string(),
-    )
-    .unwrap();
-    k.register_process(Box::new(p));
-    k.start();
     Ok(())
+}
+#[cfg(test)]
+mod test {
+    use python::PythonProcess;
+
+    #[test]
+    fn python_based_process() {
+        let mut k = kernel::Kernel::default();
+        let p = PythonProcess::new(
+            r#"async def proc():
+        print("print")
+        print("/")
+
+        print("step")
+        await step()
+
+        print("pending")
+        await pending()
+
+    wrapper()"#
+                .to_string(),
+        )
+        .unwrap();
+        k.register_process(Box::new(p));
+        k.start();
+    }
 }
